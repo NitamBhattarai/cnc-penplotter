162250184f9088cc1fb2c27b4b800fba8dc777b6
NitamBhattarai <thor57180@gmail.com>
2026-01-13 15:47:45 +0545

    third  commit

--- back-end/app.py
+++ back-end/app.py
@@ -19,5 +19,5 @@ def generate_gcode():
     return gcode, 200, {'Content-Type': 'text/plain'}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
